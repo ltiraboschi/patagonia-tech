1a1621d554f1679a26c94a5af0e789ae8f1e2fc5
Lucas Tiraboschi <ltiraboschi@gmail.com>
2021-05-12 13:13:40 -0300

    jQuery

--- js/eventos.js
+++ js/eventos.js
@@ -1,41 +1,33 @@
 //Eventos para filtrar por marca, precio y nombre
 
 $("#Apple").on("click", () => filtrarMarcas("Apple"))
-// $(`${marca}`).on("click", () => filtrarMarcas(marca))
-
 $("#Samsung").on("click", () => filtrarMarcas("Samsung"))
-
 $("#Sony").on("click", () => filtrarMarcas("Sony"))
-
 $("#Todas").on("click", () => filtrarMarcas("Todas"))
 
 $("#sortCaros").on("click", () => sortCards("Caros"))
-
 $("#sortBaratos").on("click", () => sortCards("Baratos"))
-
 $("#sortNombre").on("click", () => sortCards("Nombre"))
-
 $("#sortBorrar").on("click", () => sortCards("Borrar"))
+
+
 
 
 //Eventos del modal
 
-const carritoAbrir = document.getElementById("icono-carrito");
-const carritoCerrar = document.getElementById("carritoCerrar");
 
-const contenedorModal = document.getElementsByClassName("modal-contenedor")[0];
-const modalCarrito = document.getElementsByClassName("modal-carrito")[0];
+$("#icono-carrito").on("click", () => {
+    $(".modal-contenedor").toggleClass("modal-active");
+});
 
-carritoAbrir.addEventListener("click", () => {
-    contenedorModal.classList.toggle("modal-active");
+$("#carritoCerrar").on("click", () => {
+    $(".modal-contenedor").toggleClass("modal-active");
 });
-carritoCerrar.addEventListener("click", () => {
-    contenedorModal.classList.toggle("modal-active");
-});
-modalCarrito.addEventListener("click", (e) => {
+
+$(".modal-carrito").on("click",  (e) => {
     e.stopPropagation();
 });
 
-contenedorModal.addEventListener("click", () => {
+$(".modal-contenedor").on("click",  () => {
     carritoCerrar.click();
 });
