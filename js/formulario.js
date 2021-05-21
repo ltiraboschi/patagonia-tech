36549a2fa26cbfc52db981fd837ed3f35b4d0e61
Lucas Tiraboschi <ltiraboschi@gmail.com>
2021-05-20 23:28:03 -0300

    API Mercadopago

--- js/formulario.js
+++ js/formulario.js
@@ -53,19 +53,3 @@ function validar() {
 
 
 
-  let productos = [
-    { id: 1, nombre: 'Escuadra', precio: 323.45 },
-    { id: 2, nombre: 'Calculadora', precio: 234.56 },
-    { id: 3, nombre: 'Globo Terraqueo', precio: 45.67 },
-    { id: 4, nombre: 'Paleta Pintura', precio: 456.78 },
-    { id: 5, nombre: 'Reloj', precio: 67.89 },
-    { id: 6, nombre: 'Agenda', precio: 78.90 },
-]
-
-
-
-const a = []
-const b = 
-const c =
-const d =
-const e= 
